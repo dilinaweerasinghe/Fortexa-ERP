90faa16a3381bc6eacdeb81440177be22b33e23b
nimanthijayathilake678 <jayathilake.nimanthi99@gmail.com>
2025-07-13 10:19:02 +0530

    USER_Service:Modifications According to the Requested Changes

--- user-service/backend/src/main/java/com/shop/microservices/user_service/Controller/UserController.java
+++ user-service/backend/src/main/java/com/shop/microservices/user_service/Controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/User")
+@RequestMapping("v1/api/user")
 public class UserController {
     private final IUserService iUserService;
 

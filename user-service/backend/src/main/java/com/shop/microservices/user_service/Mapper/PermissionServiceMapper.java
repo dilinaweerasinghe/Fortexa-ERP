b3dc2e8e6b731327b77ec6867e691554aecd1a95
nimanthijayathilake678 <jayathilake.nimanthi99@gmail.com>
2025-07-14 15:02:55 +0530

    USER_SERVICE:Add PermissionMapper  for permission  management functionalities

---
+++ user-service/backend/src/main/java/com/shop/microservices/user_service/Mapper/PermissionServiceMapper.java
@@ -0,0 +1,49 @@
+package com.shop.microservices.user_service.Mapper;
+
+import com.shop.microservices.user_service.Dto.PermissionRequestDTO;
+import com.shop.microservices.user_service.Dto.PermissionResponseDTO;
+import com.shop.microservices.user_service.Model.Permission;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class PermissionServiceMapper {
+
+    //Map PermissionRequest to Permission (Save)
+    public Permission toEntity(PermissionRequestDTO permissionRequestDTO){
+        if(permissionRequestDTO == null) {
+            return null;
+        }
+
+        return Permission.builder()
+                .permission(permissionRequestDTO.getPermission())
+                .description(permissionRequestDTO.getDescription())
+                .build();
+    }
+
+    //Map Permission to PermissionResponse (fetch)
+    public PermissionResponseDTO toDto(Permission permission) {
+        if(permission == null) {
+            return null;
+        }
+        return PermissionResponseDTO.builder()
+                .permission(permission.getPermission())
+                .id(permission.getId())
+                .createdBy(permission.getCreatedBy())
+                .createdDate(LocalDateTime.from(permission.getCreatedDate()))
+                .lastModifiedBy(permission.getLastModifiedBy())
+                .lastModifiedDate(LocalDateTime.from(permission.getLastModifiedDate()))
+                .build();
+    }
+
+    //Map  all Permissions
+    List<PermissionResponseDTO> getAllPermissions(List<Permission> permissions) {
+        if(permissions == null) {
+            return null;
+        }
+
+        return permissions.stream()
+                .map(this::toDto)
+                .toList();
+    }
+}
